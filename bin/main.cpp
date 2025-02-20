d5d737d8068677472d9bee6714ef0df75ca79e7f
Flysky701 <18458871liu@gmail.com>
2025-02-21 01:26:22 +0800

    简单测试

---
+++ bin/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+// #include "String_My.h"
+
+using std::endl;
+using std::cout;
+
+int main(){
+    cout << "测试1" << std::endl;
+    
+    
+    
+    std::string test;
+    std::cin >> test;
+    cout << test << endl;
+}
