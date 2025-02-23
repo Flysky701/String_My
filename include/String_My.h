c710972f53d1c63f05bb595f6cc640f8cf1a21c7
Flysky701 <18458871liu@gmail.com>
2025-02-24 02:13:54 +0800

    添加部分函数

--- include/String_My.h
+++ include/String_My.h
@@ -64,13 +64,7 @@ public:
         return *this;
     }
 
-    char& operator[](size_t index){
-        return data[index];
-    }
-    const char& operator[](size_t index) const{
-        return data[index];
-    }
-    // 操作
+    // 操作重载
     String& operator+=(const String &other){
         if(other.len == 0)return *this;
 
@@ -107,13 +101,20 @@ public:
         len = n_len;
         data[len] = '\0';
         return *this;
+    } 
+    char& operator[](size_t index){
+        return data[index];
+    }
+    const char& operator[](size_t index) const{
+        return data[index];
     }
 
     String& operator==(const String &other){
         if(len != other.len)return false;
-        
+        return memcmp(data, other.data, len) == 0;
     }
 
+    // 常见函数操作
     size_t size(){
         return this -> len;
     }
@@ -123,7 +124,46 @@ public:
     bool empty(){
         return len == 0 ? true : false;
     }
+    void clear(){
+        data[0] = '\0';
+        len = 0;
+    }
+    
+    void insert(size_t pos, const char *str){
+        if(str == nullptr || *str == "\0")return;
+        if(pos > len)return;
+        size_t str_len = strlen(str);
+        size_t n_len = str_len + len;
+        
+        if(n_len + 1> cap)reserve(n_len * 2);
+        memmove(data + pos + str_len, data + pos, len - pos + 1);
+        memcpy(data + pos, str, str_len);
+        len = n_len;
+        data[len] = '\0';
+    }
+    void insert(size_t pos, String &other){
+        insert(pos, other.data);
+    }
+    void insert(size_t pos, char c){
+        if(pos > len)return;
+        if(len + 2 > cap)reserve((cap == 0) ? 1 : cap * 2);
+        memmove(data + pos + 1, data + pos, len - pos + 1);
+        data[pos] = c;
+        len ++;
+        data[len] = '\0';
+    }
 
+    //输入输出 
+    friend std::ostream& operator<<(std::ostream os, String& str){
+        if(str.data != nullptr)
+            os << str.data; 
+        else os << "";
+    }
+    friend std::itream& operator>>(std::istream io)
+
+   
+
+    // 内存管理
     void reserve(size_t n_cap) {
         if (n_cap <= cap) return;
         char* n_data = new char[n_cap]; 
@@ -134,12 +174,10 @@ public:
         data = n_data;
         cap = n_cap;
     }
-
-
-
-
-
-    // += / == / + 
+    void shrink_to_fit(){
+        if(cap > len + 1)
+            reserve(len + 1);
+    }
     
 private:
     char* data;
